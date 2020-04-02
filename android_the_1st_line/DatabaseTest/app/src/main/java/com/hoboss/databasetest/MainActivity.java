e7f162a350cb69f92271cd6e6af71a7cbca97e7a
hobossa <hoboss@gmail.com>
2020-04-03 00:46:01 +0800

    add DatabaseTest

---
+++ android_the_1st_line/DatabaseTest/app/src/main/java/com/hoboss/databasetest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hoboss.databasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper myDatabaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDatabaseHelper = new MyDatabaseHelper(this, "book_store.db",
+                null, 1);
+        Button createDatabase = (Button)findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                myDatabaseHelper.getWritableDatabase();
+            }
+        });
+    }
+}
