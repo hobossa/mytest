705c82d4ccb26ad3eb605b7dc7952f47500aa035
hobossa <hoboss@gmail.com>
2020-04-08 19:45:11 +0800

    PlayAudioTest done

--- android_the_1st_line/PlayAudioTest/app/src/main/java/com/hoboss/playaudiotest/MainActivity.java
+++ android_the_1st_line/PlayAudioTest/app/src/main/java/com/hoboss/playaudiotest/MainActivity.java
@@ -1,14 +1,94 @@
 package com.hoboss.playaudiotest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(this);
+        Button pause = (Button) findViewById(R.id.pause);
+        pause.setOnClickListener(this);
+        Button stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(this);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            initMediaPlayer();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.play:
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();;
+                }
+                break;
+            case R.id.pause:
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.stop:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initMediaPlayer();
+                } else {
+                    Toast.makeText(this, "Cannot get WRITE_EXTRA_STORAGE permission",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void initMediaPlayer() {
+        try {
+            File file = new File(Environment.getExternalStorageState(), "music.mp3");
+            mediaPlayer.setDataSource(file.getPath());
+            mediaPlayer.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
