691f438ae2f9c2cc13303d2daa1917ed4987e3ed
hobossa <hoboss@gmail.com>
2020-04-12 23:38:27 +0800

    5.1 done

--- android_fundamentals/scorekeeper/app/src/main/java/com/hoboss/scorekeeper/MainActivity.java
+++ android_fundamentals/scorekeeper/app/src/main/java/com/hoboss/scorekeeper/MainActivity.java
@@ -1,12 +1,19 @@
 package com.hoboss.scorekeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
     private int mScore1;
     private int mScore2;
     private TextView mScoreText1;
@@ -17,23 +24,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mScoreText1 = (TextView) findViewById(R.id.score_1);
-        mScoreText2 = (TextView) findViewById(R.id.score_2);
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+            //Set the score text views
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 
-    public void decreaseScore(View view) {
-        switch (view.getId()) {
-            case R.id.decreaseTeam1:
-                mScore1--;
-                mScoreText1.setText(String.valueOf(mScore1));
-                break;
-            case R.id.decreaseTeam2:
-                mScore2--;
-                mScoreText2.setText(String.valueOf(mScore2));
-                break;
-            default:
-                break;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        // Save the scores.
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.string.app_name);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
         }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.night_mode) {
+            // Get the night mode state of the app.
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            //Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Recreate the activity for the theme change to take effect.
+            recreate();
+        }
+        return true;
     }
 
     public void increaseScore(View view) {
@@ -44,6 +86,21 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.increaseTeam2:
                 mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void decreaseScore(View view) {
+        switch (view.getId()) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
                 mScoreText2.setText(String.valueOf(mScore2));
                 break;
             default:
