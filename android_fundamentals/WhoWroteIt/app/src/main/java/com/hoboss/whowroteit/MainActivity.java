97d3a484e9cb5dbc4b24a5ef24fa1d560af0efaf
hobossa <hoboss@gmail.com>
2020-04-17 17:24:30 +0800

    1. hide keyboard automatically 2. giving a prompt when the network is not working or the query string is null

--- android_fundamentals/WhoWroteIt/app/src/main/java/com/hoboss/whowroteit/MainActivity.java
+++ android_fundamentals/WhoWroteIt/app/src/main/java/com/hoboss/whowroteit/MainActivity.java
@@ -2,8 +2,12 @@ package com.hoboss.whowroteit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,13 +21,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBookInput = (EditText)findViewById(R.id.bookInput);
-        mTitleText = (TextView)findViewById(R.id.titleText);
-        mAuthorText = (TextView)findViewById(R.id.authorText);
+        mBookInput = (EditText) findViewById(R.id.bookInput);
+        mTitleText = (TextView) findViewById(R.id.titleText);
+        mAuthorText = (TextView) findViewById(R.id.authorText);
     }
 
     public void searchBooks(View view) {
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
     }
 }
