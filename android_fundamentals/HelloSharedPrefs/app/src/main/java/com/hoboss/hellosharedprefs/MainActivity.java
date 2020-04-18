ef3896584f70d48ffb028514ffacd7ea93ce43ac
hobossa <hoboss@gmail.com>
2020-04-18 22:33:35 +0800

    new HelloSharedPrefs

---
+++ android_fundamentals/HelloSharedPrefs/app/src/main/java/com/hoboss/hellosharedprefs/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hoboss.hellosharedprefs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Current count
+    private int mCount = 0;
+    // Current background color
+    private int mColor;
+    // Text view to display both count and color
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views, color
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+
+        // Restore the saved instance state.
+        if (savedInstanceState != null) {
+
+            mCount = savedInstanceState.getInt(COUNT_KEY);
+            if (mCount != 0) {
+                mShowCountTextView.setText(String.format("%s", mCount));
+            }
+
+            mColor = savedInstanceState.getInt(COLOR_KEY);
+            mShowCountTextView.setBackgroundColor(mColor);
+        }
+    }
+}
