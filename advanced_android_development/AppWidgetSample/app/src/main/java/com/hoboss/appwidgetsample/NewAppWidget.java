c0a8dc0e57646207ed887422ee2be6417b7b52f9
hobossa <hoboss@gmail.com>
2020-04-26 15:41:11 +0800

    AppWidgetSample done

--- advanced_android_development/AppWidgetSample/app/src/main/java/com/hoboss/appwidgetsample/NewAppWidget.java
+++ advanced_android_development/AppWidgetSample/app/src/main/java/com/hoboss/appwidgetsample/NewAppWidget.java
@@ -1,22 +1,51 @@
 package com.hoboss.appwidgetsample;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
+    private static final String mSharedPrefFile = "com.hoboss.appwidgetsample";
+    private static final String COUNT_KEY = "count";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(
+                mSharedPrefFile, 0);
+        int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
+        count++;
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+
+        String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+        views.setTextViewText(R.id.appwidget_update,
+                context.getResources().getString(R.string.date_count_format, count, dateString));
+
+        SharedPreferences.Editor prefEditor = prefs.edit();
+        prefEditor.putInt(COUNT_KEY + appWidgetId, count);
+        prefEditor.apply();
+
+        Intent intentUpdate = new Intent(context, NewAppWidget.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] idArray = new int[]{appWidgetId}; // only updates myself and not the other widget.
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(
+                context, appWidgetId, intentUpdate,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -28,16 +57,6 @@ public class NewAppWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 }
 
