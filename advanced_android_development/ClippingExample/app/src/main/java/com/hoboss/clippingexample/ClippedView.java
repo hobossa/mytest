959d3505f0e957be2b8ee4410f166e39e3aeb2d7
hobossa <hoboss@gmail.com>
2020-05-05 22:57:42 +0800

    fix

--- advanced_android_development/ClippingExample/app/src/main/java/com/hoboss/clippingexample/ClippedView.java
+++ advanced_android_development/ClippingExample/app/src/main/java/com/hoboss/clippingexample/ClippedView.java
@@ -54,50 +54,66 @@ public class ClippedView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // Draw a rectangle that uses the difference between two
-        // clipping rectangles to create a picture frame effect.
+        // Fill the unclipped canvas with gray background color.
+        canvas.drawColor(Color.GRAY);
+        // Save the modified canvas to a private stack.
+        canvas.save();
+        // Instead of drawing the rectangle with new coordinates every time,
+        // move the origin of the Canvas and draw in the "same" location.
+        // This is more efficient.
+        canvas.translate(mColumnOne, mRowOne);
+        drawClippedRectangle(canvas);
+        canvas.restore();
+
+        // Context maintains a stack of drawing states, including
+        // currently applied transformations and clipping regions.
+        // Pop the states with the transformations from the stack
+        // to undo them.
+        // In this example, after moving the origin and drawing
+        // the next rectangle, instead of translating again from the
+        // new origin, reset to the original (top-left corner)
+        // origin and translate again from there.
         canvas.save();
         // Move the origin to the right for the next rectangle.
         canvas.translate(mColumnnTwo, mRowOne);
         // Use the subtraction of two clipping rectangles to create a frame.
-        canvas.clipRect(2 * mRectInset, 2 * mRectInset,
-                mClipRectRight - 2 * mRectInset, mClipRectBottom - 2 * mRectInset);
+        canvas.clipRect(2*mRectInset, 2*mRectInset,
+                mClipRectRight-2*mRectInset, mClipRectBottom-2*mRectInset);
         // The method clipRect(float, float, float, float, Region.Op
         // .DIFFERENCE) was deprecated in API level 26. The recommended
         // alternative method is clipOutRect(float, float, float, float),
         // which is currently available in API level 26 and higher.
-        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
-            canvas.clipRect(4 * mRectInset, 4 * mRectInset,
-                    mClipRectRight - 4 * mRectInset, mClipRectBottom - 4 * mRectInset,
+        if(android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            canvas.clipRect(4*mRectInset, 4*mRectInset,
+                    mClipRectRight-4*mRectInset, mClipRectBottom-4*mRectInset,
                     Region.Op.DIFFERENCE);
-        else {
-            canvas.clipOutRect(4 * mRectInset, 4 * mRectInset,
-                    mClipRectRight - 4 * mRectInset,
-                    mClipRectBottom - 4 * mRectInset);
+        else{
+            canvas.clipOutRect(4*mRectInset, 4*mRectInset,
+                    mClipRectRight-4*mRectInset,
+                    mClipRectBottom-4*mRectInset);
         }
+
 
         drawClippedRectangle(canvas);
         canvas.restore();
 
-        // Draw a rectangle that uses a circular clipping region
-        // created from a circular path.
+        // Circular clipping path.
         canvas.save();
         canvas.translate(mColumnOne, mRowTwo);
         // Clears any lines and curves from the path but unlike reset(),
         // keeps the internal data structure for faster reuse.
         mPath.rewind();
-        mPath.addCircle(mCircleRadius, mClipRectBottom - mCircleRadius,
+        mPath.addCircle(mCircleRadius, mClipRectBottom-mCircleRadius,
                 mCircleRadius, Path.Direction.CCW);
         // The method clipPath(path, Region.Op.DIFFERENCE) was deprecated in
         // API level 26. The recommended alternative method is
-        // clipOutPath(Path), which is currently available in
-        // API level 26 and higher.
+        // clipOutPath(Path), which is currently available in API level 26
+        // and higher.
         if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             canvas.clipPath(mPath, Region.Op.DIFFERENCE);
         } else {
             canvas.clipOutPath(mPath);
         }
-
         drawClippedRectangle(canvas);
         canvas.restore();
 
@@ -105,8 +121,8 @@ public class ClippedView extends View {
         canvas.save();
         canvas.translate(mColumnnTwo, mRowTwo);
         canvas.clipRect(mClipRectLeft, mClipRectTop,
-                mClipRectRight - mSmallRectOffset,
-                mClipRectBottom - mSmallRectOffset);
+                mClipRectRight-mSmallRectOffset,
+                mClipRectBottom-mSmallRectOffset);
         // The method clipRect(float, float, float, float, Region.Op
         // .INTERSECT) was deprecated in API level 26. The recommended
         // alternative method is clipRect(float, float, float, float), which
@@ -128,13 +144,13 @@ public class ClippedView extends View {
         canvas.save();
         canvas.translate(mColumnOne, mRowThree);
         mPath.rewind();
-        mPath.addCircle(mClipRectLeft + mRectInset + mCircleRadius,
-                mClipRectTop + mCircleRadius + mRectInset,
+        mPath.addCircle(mClipRectLeft+mRectInset+mCircleRadius,
+                mClipRectTop+mCircleRadius+mRectInset,
                 mCircleRadius, Path.Direction.CCW);
-        mPath.addRect(mClipRectRight / 2 - mCircleRadius,
-                mClipRectTop + mCircleRadius + mRectInset,
-                mClipRectRight / 2 + mCircleRadius,
-                mClipRectBottom - mRectInset, Path.Direction.CCW);
+        mPath.addRect(mClipRectRight/2-mCircleRadius,
+                mClipRectTop+mCircleRadius+mRectInset,
+                mClipRectRight/2+mCircleRadius,
+                mClipRectBottom-mRectInset,Path.Direction.CCW);
         canvas.clipPath(mPath);
         drawClippedRectangle(canvas);
         canvas.restore();
@@ -143,8 +159,8 @@ public class ClippedView extends View {
         canvas.save();
         canvas.translate(mColumnnTwo, mRowThree);
         mPath.rewind();
-        mPath.addRoundRect(mRectF, (float) mClipRectRight / 4,
-                (float) mClipRectRight / 4, Path.Direction.CCW);
+        mPath.addRoundRect(mRectF, (float)mClipRectRight/4,
+                (float)mClipRectRight/4, Path.Direction.CCW);
         canvas.clipPath(mPath);
         drawClippedRectangle(canvas);
         canvas.restore();
@@ -153,11 +169,12 @@ public class ClippedView extends View {
         canvas.save();
         // Move the origin to the right for the next rectangle.
         canvas.translate(mColumnOne, mRowFour);
-        canvas.clipRect(2 * mRectInset, 2 * mRectInset,
-                mClipRectRight - 2 * mRectInset,
-                mClipRectBottom - 2 * mRectInset);
+        canvas.clipRect(2*mRectInset, 2*mRectInset,
+                mClipRectRight-2*mRectInset,
+                mClipRectBottom-2*mRectInset);
         drawClippedRectangle(canvas);
         canvas.restore();
+
 
         // Draw text with a translate transformation applied.
         canvas.save();
@@ -167,7 +184,8 @@ public class ClippedView extends View {
         // Apply transformation to canvas.
         canvas.translate(mColumnnTwo, mTextRow);
         // Draw text.
-        canvas.drawText(getContext().getString(R.string.translated), 0, 0, mPaint);
+        canvas.drawText(
+                getContext().getString(R.string.translated), 0, 0, mPaint);
         canvas.restore();
 
         // Draw text with a translate and skew transformations applied.
@@ -178,7 +196,8 @@ public class ClippedView extends View {
         canvas.translate(mColumnnTwo, mTextRow);
         // Apply skew transformation.
         canvas.skew(0.2f, 0.3f);
-        canvas.drawText(getContext().getString(R.string.skewed), 0, 0, mPaint);
+        canvas.drawText(
+                getContext().getString(R.string.skewed), 0, 0, mPaint);
         canvas.restore();
     }
 
