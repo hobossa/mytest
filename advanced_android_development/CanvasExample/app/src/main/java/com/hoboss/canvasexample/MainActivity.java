1ad29b1b094cac252cc87ecaa244bc4ec6c20a12
hobossa <hoboss@gmail.com>
2020-05-05 22:55:00 +0800

    canvas clipping test done

---
+++ advanced_android_development/CanvasExample/app/src/main/java/com/hoboss/canvasexample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hoboss.canvasexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        MyCanvasView myCanvasView;
+        // No XML file; just one custom view created programmatically.
+        myCanvasView = new MyCanvasView(this);
+        // Request the full available screen for layout.
+        myCanvasView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(myCanvasView);
+    }
+
+
+}
