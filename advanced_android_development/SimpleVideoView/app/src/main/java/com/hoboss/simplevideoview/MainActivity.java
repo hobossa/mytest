77f5500561d91bee67a5ef1189b62701e27d2a84
hobossa <hoboss@gmail.com>
2020-05-06 15:05:00 +0800

    modify the SimpleVideoView app to play a video from a URL on the internet

--- advanced_android_development/SimpleVideoView/app/src/main/java/com/hoboss/simplevideoview/MainActivity.java
+++ advanced_android_development/SimpleVideoView/app/src/main/java/com/hoboss/simplevideoview/MainActivity.java
@@ -7,20 +7,27 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.webkit.URLUtil;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String VIDEO_SAMPLE = "tacoma_narrows";
+    // private static final String VIDEO_SAMPLE = "tacoma_narrows";
+    private static final String VIDEO_SAMPLE =
+            "https://developers.google.com/training/images/tacoma_narrows.mp4";
     private static final String PLAYBACK_TIME = "play_time";
     private VideoView mVideoView;
     private int mCurrentPosition = 0;
+    private TextView mBufferingTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBufferingTextView = findViewById(R.id.buffering_textview);
 
         mVideoView = findViewById(R.id.videoview);
         if (savedInstanceState != null) {
@@ -79,20 +86,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Uri getMedia(String mediaName) {
-        return Uri.parse("android.resource://" + getPackageName() +
-                "/raw/" + mediaName);
+        if (URLUtil.isValidUrl(mediaName)) {
+            // media name is an external URL
+            return Uri.parse(mediaName);
+        } else { // media name is a raw resource embedded in the app
+            return Uri.parse("android.resource://" + getPackageName() +
+                    "/raw/" + mediaName);
+        }
     }
 
     private void initializePlayer() {
+        mBufferingTextView.setVisibility(VideoView.VISIBLE);
         Uri videoUri = getMedia(VIDEO_SAMPLE);
         mVideoView.setVideoURI(videoUri);
-        if (mCurrentPosition > 0) {
-            mVideoView.seekTo(mCurrentPosition);
-        } else {
-            // Skipping to 1 shows the first frame of the video.
-            mVideoView.seekTo(1);
-        }
-        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mBufferingTextView.setVisibility(VideoView.INVISIBLE);
+
+                if (mCurrentPosition > 0) {
+                    mVideoView.seekTo(mCurrentPosition);
+                } else {
+                    mVideoView.seekTo(1);
+                }
+                mVideoView.start();
+            }
+        });
         mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
