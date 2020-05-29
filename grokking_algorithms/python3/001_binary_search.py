62699756f6cd09427642edc4ff3417d43feac251
hobossa <hoboss@gmail.com>
2020-05-29 20:12:04 +0800

    binary_search

---
+++ grokking_algorithms/python3/001_binary_search.py
@@ -0,0 +1,11 @@
+# coding=utf8
+import sys
+
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    if sys.version_info[0] < 3:
+        raise Exception("Python 3 or a more recent version is required.")
+    run()
