37b4e801b8b9c21b9d5aaf1122a3b5cebec01e1b
hobossa <hoboss@gmail.com>
2020-06-03 16:12:36 +0800

    dynamic subsequence done

--- grokking_algorithms/python3/008_greedy_algorithm.py
+++ grokking_algorithms/python3/008_greedy_algorithm.py
@@ -1,4 +1,4 @@
-# coding=utf8
+# coding=utf-8
 import sys
 
 # You pass an array in, and it gets converted to a set.
--- grokking_algorithms/python3/009_dynamic_subsequence.py
+++ grokking_algorithms/python3/009_dynamic_subsequence.py
@@ -1,15 +1,50 @@
-# coding=utf8
+# coding=utf-8
 import sys
 
-class Cell:
-    pass
 
 def subsequence(strA, strB):
-    pass
+    #    s t r B
+    #   +-+-+-+-+
+    # s |1 1 1 1
+    # t |1 2 2 2
+    # r |1 2 3 3
+    # A |1 2 3 3
+    row = len(strA)
+    col = len(strB)
+    value_matrix = [[0 for j in range(col)] for i in range(row)]
+    max_value = 0
+    max_value_coordinates = []
+    for i in range(row):
+        for j in range(col):
+            if strA[i] == strB[j]:
+                if i == 0 or j == 0:
+                    value_matrix[i][j] = 1
+                else:
+                    value_matrix[i][j] = value_matrix[i - 1][j - 1] + 1
+                if max_value < value_matrix[i][j]:
+                    max_value = value_matrix[i][j]
+                    max_value_coordinates = [(i, j)]
+                elif max_value == value_matrix[i][j]:
+                    max_value_coordinates.append((i, j))
+            else:
+                if i == 0 and j == 0:
+                    value_matrix[i][j] = 0
+                elif i == 0 and j > 0:
+                    value_matrix[i][j] = value_matrix[i][j - 1]
+                elif i > 0 and j == 0:
+                    value_matrix[i][j] = value_matrix[i - 1][j]
+                else:
+                    value_matrix[i][j] = max(value_matrix[i - 1][j],
+                                             value_matrix[i][j - 1])
+
+    for v in value_matrix:
+        print(v)
+
+    # toDo: we can get max sequences with max_value_coordinates
 
 
 def run():
-    pass
+    subsequence("fishf", "pfoshe")
 
 
 if __name__ == "__main__":
