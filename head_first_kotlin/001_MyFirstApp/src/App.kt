16775c7618298a569a13aed3d282dcdd23ed12ce
hobossa <hoboss@gmail.com>
2020-06-15 23:15:40 +0800

    update App.kt

--- head_first_kotlin/001_MyFirstApp/src/App.kt
+++ head_first_kotlin/001_MyFirstApp/src/App.kt
@@ -1,3 +1,29 @@
 fun main(args: Array<String>) {
     println("Pow!");
+
+    // Do something (statements)
+    var x = 3
+    val name = "Cormoran"
+    x *= 10
+    print("x is $x.")
+
+    // loops
+    // x = 30
+    while (x > 20) {
+        x -= 1
+    }
+    for ( i in 1..10){
+        x += 1
+        print(" x is now $x.")
+    }
+
+    // condition
+    if (x == 20) {
+        println(" x must be 20.")
+    } else {
+        println(" x isn't 20.")
+    }
+    if (name.equals("Cormoran")) {
+        println("$name Strike")
+    }
 }
