bbaa695f31c573c7b9e0f3cc16f4f9210e09a78d
hobossa <hoboss@gmail.com>
2020-06-27 22:25:26 +0800

    Hello Kotlin done

---
+++ kotlin_bootcamp_for_programmers/001_HelloKotlin/src/HelloKotlin.kt
@@ -0,0 +1,10 @@
+// 1. Select Tools > Kotlin > Kotlin REPL to open the REPL.
+
+// 2. Type or paste the code below into the REPL.
+// fun printHello() {
+//     println("Hello World")
+// }
+//
+// printHello()
+
+// 3. Press Control+Enter (Command+Enter on a Mac). You should see Hello World
