3c7b2a51cce9f0c085d0ad2099289199e32bbecc
hobossa <hoboss@gmail.com>
2020-08-04 15:35:29 +0800

    done Advanced Android in Kotlin 01.2: Android Firebase Cloud Messaging

--- advanced_android_in_kotlin/android-kotlin-notifications-fcm/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ advanced_android_in_kotlin/android-kotlin-notifications-fcm/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -31,24 +31,36 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
-    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: ${remoteMessage?.from}")
+        Log.d(TAG, "From: ${remoteMessage.from}")
 
         // TODO Step 3.5 check messages for data
         // Check if message contains a data payload.
-
+        remoteMessage.data?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
 
         // TODO Step 3.6 check messages for notification and call sendNotification
         // Check if message contains a notification payload.
-
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body as String)
+        }
     }
     // [END receive_message]
 
     //TODO Step 3.2 log registration token
     // [START on_new_token]
-
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
     // [END on_new_token]
+
 
     /**
      * Persist token to third-party (your app) servers.
@@ -65,7 +77,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      * @param messageBody FCM message body received.
      */
     private fun sendNotification(messageBody: String) {
-        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
         notificationManager.sendNotification(messageBody, applicationContext)
     }
 
